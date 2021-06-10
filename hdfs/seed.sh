05550558ba26b726effe49e7bb1afd0f9070d0c0
Fernanda Vega <fernanda.vega@thoughtworks.com>
2021-06-10 10:20:00 -0500

    4| Megan, Marco, Fernanda| initial implementation for raw data saver for France

--- hdfs/seed.sh
+++ hdfs/seed.sh
@@ -8,6 +8,8 @@ $hadoop_path fs -fs hdfs://$hdfs_server -mkdir -p /tw/rawData/stationInformation
 && $hadoop_path fs -fs hdfs://$hdfs_server -mkdir -p /tw/rawData/stationStatus/data \
 && $hadoop_path fs -fs hdfs://$hdfs_server -mkdir -p /tw/rawData/stationDataSF/checkpoints \
 && $hadoop_path fs -fs hdfs://$hdfs_server -mkdir -p /tw/rawData/stationDataSF/data \
+&& $hadoop_path fs -fs hdfs://$hdfs_server -mkdir -p /tw/rawData/stationDataFR_MS/checkpoints \
+&& $hadoop_path fs -fs hdfs://$hdfs_server -mkdir -p /tw/rawData/stationDataFR_MS/data \
 && $hadoop_path fs -fs hdfs://$hdfs_server -mkdir -p /tw/rawData/stationDataNYC/checkpoints \
 && $hadoop_path fs -fs hdfs://$hdfs_server -mkdir -p /tw/stationMart/checkpoints \
 && $hadoop_path fs -fs hdfs://$hdfs_server -mkdir -p /tw/stationMart/data
