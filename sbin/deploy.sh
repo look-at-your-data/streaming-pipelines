9aef2b8ac53372fad91e2e9dcc8e2e156eca1186
Marco Barragan <mbarraga@thoughtworks.com>
2021-06-10 12:54:02 -0400

    3 | Marco, Li | fix variable name in deploy.sh script for france marseille

--- sbin/deploy.sh
+++ sbin/deploy.sh
@@ -93,7 +93,7 @@ echo "====Deploy Producers===="
 nohup java -jar /tmp/tw-citibike-apis-producer0.1.0.jar --spring.profiles.active=\${station_information} --kafka.brokers=kafka.${TRAINING_COHORT}.training:9092 1>/tmp/\${station_information}.log 2>/tmp/\${station_information}.error.log &
 nohup java -jar /tmp/tw-citibike-apis-producer0.1.0.jar --spring.profiles.active=\${station_san_francisco} --producer.topic=station_data_sf --kafka.brokers=kafka.${TRAINING_COHORT}.training:9092 1>/tmp/\${station_san_francisco}.log 2>/tmp/\${station_san_francisco}.error.log &
 nohup java -jar /tmp/tw-citibike-apis-producer0.1.0.jar --spring.profiles.active=\${station_status} --kafka.brokers=kafka.${TRAINING_COHORT}.training:9092 1>/tmp/\${station_status}.log 2>/tmp/\${station_status}.error.log &
-nohup java -jar /tmp/tw-citibike-apis-producer0.1.0.jar --spring.profiles.active=\${station_france_marseille} --kafka.brokers=kafka.${TRAINING_COHORT}.training:9092 1>/tmp/\${station_fr_ms}.log 2>/tmp/\${station_fr_ms}.error.log &
+nohup java -jar /tmp/tw-citibike-apis-producer0.1.0.jar --spring.profiles.active=\${station_fr_ms} --kafka.brokers=kafka.${TRAINING_COHORT}.training:9092 1>/tmp/\${station_fr_ms}.log 2>/tmp/\${station_fr_ms}.error.log &
 
 echo "====Producers Deployed===="
 EOF
