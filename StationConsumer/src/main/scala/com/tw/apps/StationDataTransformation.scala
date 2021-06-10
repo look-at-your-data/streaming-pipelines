5bfb06293d3a03bda55d08c830b1c9b560df517d
Marco Barragan <mbarraga@thoughtworks.com>
2021-06-10 11:38:04 -0400

    3 | Megan, Marco, Fernanda, Li, Jeff| create France producer; update Station Consumer to aggregrate France data to CSV

--- StationConsumer/src/main/scala/com/tw/apps/StationDataTransformation.scala
+++ StationConsumer/src/main/scala/com/tw/apps/StationDataTransformation.scala
@@ -18,8 +18,13 @@ object StationDataTransformation {
     extractSFStationStatus(payload)
   }
 
-  private def extractSFStationStatus(payload: Any) = {
+  val franceMarseilleToStationStatus: String => Seq[StationData] = raw_payload => {
+    val json = JSON.parseFull(raw_payload)
+    val payload = json.get.asInstanceOf[Map[String, Any]]("payload")
+    extractFranceMarseilleStationStatus(payload)
+  }
 
+  private def extractSFStationStatus(payload: Any) = {
     val network: Any = payload.asInstanceOf[Map[String, Any]]("network")
 
     val stations: Any = network.asInstanceOf[Map[String, Any]]("stations")
@@ -31,6 +36,27 @@ object StationDataTransformation {
           x("empty_slots").asInstanceOf[Double].toInt,
           x("extra").asInstanceOf[Map[String, Any]]("renting").asInstanceOf[Double] == 1,
           x("extra").asInstanceOf[Map[String, Any]]("returning").asInstanceOf[Double] == 1,
+          Instant.from(DateTimeFormatter.ISO_INSTANT.parse(x("timestamp").asInstanceOf[String])).getEpochSecond,
+          x("id").asInstanceOf[String],
+          x("name").asInstanceOf[String],
+          x("latitude").asInstanceOf[Double],
+          x("longitude").asInstanceOf[Double]
+        )
+      })
+  }
+
+  private def extractFranceMarseilleStationStatus(payload: Any) = {
+    val network: Any = payload.asInstanceOf[Map[String, Any]]("network")
+
+    val stations: Any = network.asInstanceOf[Map[String, Any]]("stations")
+
+    stations.asInstanceOf[Seq[Map[String, Any]]]
+      .map(x => {
+        StationData(
+          x("free_bikes").asInstanceOf[Double].toInt,
+          x("empty_slots").asInstanceOf[Double].toInt,
+          is_renting = true,
+          is_returning = true,
           Instant.from(DateTimeFormatter.ISO_INSTANT.parse(x("timestamp").asInstanceOf[String])).getEpochSecond,
           x("id").asInstanceOf[String],
           x("name").asInstanceOf[String],
@@ -53,6 +79,15 @@ object StationDataTransformation {
     import spark.implicits._
 
     jsonDF.select(from_json($"raw_payload", ScalaReflection.schemaFor[StationData].dataType) as "status")
+      .select($"status.*")
+  }
+
+  def franceMarseilleStationStatusJson2DF(jsonDF: DataFrame, spark: SparkSession): DataFrame = {
+    val toStatusFn: UserDefinedFunction = udf(franceMarseilleToStationStatus)
+
+    import spark.implicits._
+
+    jsonDF.select(explode(toStatusFn(jsonDF("raw_payload"))) as "status")
       .select($"status.*")
   }
 }
--- StationConsumer/src/test/scala/com/tw/apps/StationDataTransformationTest.scala
+++ StationConsumer/src/test/scala/com/tw/apps/StationDataTransformationTest.scala
@@ -1,6 +1,6 @@
 package com.tw.apps
 
-import StationDataTransformation.nycStationStatusJson2DF
+import StationDataTransformation.{franceMarseilleStationStatusJson2DF, nycStationStatusJson2DF}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.catalyst.ScalaReflection
 import org.scalatest._
@@ -65,6 +65,86 @@ class StationDataTransformationTest extends FeatureSpec with Matchers with Given
       row1.get(6) should be("Atlantic Ave & Fort Greene Pl")
       row1.get(7) should be(40.68382604)
       row1.get(8) should be(-73.97632328)
+    }
+
+    scenario("Transform france marseille station data frame") {
+      val docksAvailable = 19
+      val bikesAvailable = 0
+      val timestamp = "2021-06-08T20:14:49.042000Z"
+      val stationId = "686e48654a218c70daf950a4e893e5b0"
+      val latitude = 43.25402727813068
+      val longitude = 5.401873594694653
+      val name = "8149-391 MICHELET"
+      val isRenting = true
+      val isReturning = true
+
+      val testStationData =
+        s"""{
+              "payload": {
+                "network": {
+                  "stations": [
+                    {
+                      "empty_slots": $docksAvailable,
+                      "extra": {
+                            "address": "391 MICHELET - 391 BOULEVARD MICHELET",
+                            "banking": true,
+                            "bonus": false,
+                            "last_update": 1623183041000,
+                            "slots": 19,
+                            "status": "OPEN",
+                            "uid": 8149
+                      },
+                      "free_bikes": $bikesAvailable,
+                      "id": "$stationId",
+                      "latitude": $latitude,
+                      "longitude": $longitude,
+                      "name": "$name",
+                      "timestamp": "$timestamp"
+                    }
+                  ]
+                }
+              }
+            }"""
+
+      val schema = ScalaReflection.schemaFor[StationData].dataType //.asInstanceOf[StructType]
+
+      Given("Sample data for station_status")
+      val testDF1 = Seq(testStationData).toDF("raw_payload")
+
+
+      When("Transformations are applied")
+      val resultDF1 = testDF1.transform(franceMarseilleStationStatusJson2DF(_, spark))
+
+      Then("Useful columns are extracted")
+      resultDF1.schema.fields(0).name should be("bikes_available")
+      resultDF1.schema.fields(0).dataType.typeName should be("integer")
+      resultDF1.schema.fields(1).name should be("docks_available")
+      resultDF1.schema.fields(1).dataType.typeName should be("integer")
+      resultDF1.schema.fields(2).name should be("is_renting")
+      resultDF1.schema.fields(2).dataType.typeName should be("boolean")
+      resultDF1.schema.fields(3).name should be("is_returning")
+      resultDF1.schema.fields(3).dataType.typeName should be("boolean")
+      resultDF1.schema.fields(4).name should be("last_updated")
+      resultDF1.schema.fields(4).dataType.typeName should be("long")
+      resultDF1.schema.fields(5).name should be("station_id")
+      resultDF1.schema.fields(5).dataType.typeName should be("string")
+      resultDF1.schema.fields(6).name should be("name")
+      resultDF1.schema.fields(6).dataType.typeName should be("string")
+      resultDF1.schema.fields(7).name should be("latitude")
+      resultDF1.schema.fields(7).dataType.typeName should be("double")
+      resultDF1.schema.fields(8).name should be("longitude")
+      resultDF1.schema.fields(8).dataType.typeName should be("double")
+
+      val row1 = resultDF1.toJavaRDD.first()
+      row1.get(0) should be(bikesAvailable)
+      row1.get(1) should be(docksAvailable)
+      row1.get(2) shouldBe isRenting
+      row1.get(3) shouldBe isReturning
+      row1.get(4) should be(1623183289)
+      row1.get(5) should be(stationId)
+      row1.get(6) should be(name)
+      row1.get(7) should be(latitude)
+      row1.get(8) should be(longitude)
     }
   }
 }
