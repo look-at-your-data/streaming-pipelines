873be72c45f1a138293c185b42011da05bc7cc9e
BugDiver <vinaysh@thoughtworks.com>
2021-08-02 17:30:37 +0530

    | Vinay, Jayashree, Sonali | 3 | Add Producer for bike data from france.
    Signed-off-by: BugDiver <vinaysh@thoughtworks.com>

--- sbin/deploy.sh
+++ sbin/deploy.sh
@@ -77,6 +77,7 @@ function kill_process {
 station_information="station-information"
 station_status="station-status"
 station_san_francisco="station-san-francisco"
+station_france="station-france"
 
 
 echo "====Kill running producers===="
@@ -84,6 +85,7 @@ echo "====Kill running producers===="
 kill_process \${station_information}
 kill_process \${station_status}
 kill_process \${station_san_francisco}
+kill_process \${station_france}
 
 echo "====Runing Producers Killed===="
 
