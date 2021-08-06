2bb687e09ec2506563152755c5ef3212ac639c50
BugDiver <vinaysh@thoughtworks.com>
2021-08-06 10:57:58 +0530

    | Vinay, Kritika | 10 | Add poetry to manage dependencies.
    Signed-off-by: BugDiver <vinaysh@thoughtworks.com>

---
+++ airflow/test/validate_mart_data_test.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestValidateMartData(unittest.TestCase):
+    def test_validate_mart_data(self):
+        self.assertEqual(1, 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
